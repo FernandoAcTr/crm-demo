665aa7ebfc92119d4164644d693517c03d04e5d0
Fernando Acosta <fernandodeveloper99@gmail.com>
2022-05-16 16:55:50 -0500

    Purple react Admin integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './assets/scss/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/index.css'
 
